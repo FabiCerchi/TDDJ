729e10140e41e894d21d363457766982cacf4f9a
angelfcerchi <Sorete2340>
2022-11-11 16:23:24 -0300

    first commit

---
+++ src/tdd_tp/ejercicio8/CaculadoraPalindromo.java
@@ -0,0 +1,56 @@
+package tdd_tp.ejercicio8;
+
+import java.util.ArrayList;
+
+public class CaculadoraPalindromo {
+    public int cambiarBase(int numero){
+        int diviendo = numero;
+        ArrayList<String> binarioInvertido = new ArrayList<String>();
+        do {
+            if (diviendo%2 == 0){
+                binarioInvertido.add(String.valueOf(0));
+                diviendo = diviendo/2;
+            } else if (diviendo%2 == 1) {
+                binarioInvertido.add(String.valueOf(1));
+                diviendo = diviendo/2;
+            }
+        }while (diviendo>0);
+        ArrayList<String> binario = reverseArr(binarioInvertido);
+        String result = "";
+        for (String str:binario){
+            result+=str;
+        }
+        return Integer.parseInt(result);
+    }
+    public  ArrayList<String> reverseArr(ArrayList<String> arr){
+        ArrayList<String> arrAux = new ArrayList<>();
+        int iArr = 0;
+        for (int i = arr.size()-1; i >= 0; i--){
+            arrAux.add(iArr,arr.get(i));
+            iArr++;
+        }
+        return arrAux;
+    }
+
+    public boolean esPalindromoBinario(long numero){
+        String binarioUno = Long.toBinaryString(numero);
+        String binarioDos = new StringBuilder(binarioUno).reverse().toString();
+        return binarioUno.equals(binarioDos);
+    }
+    public  boolean esPalindromoDecimal(long numero){
+        String decimalDos = new StringBuilder(String.valueOf(numero)).reverse().toString();
+        return Long.valueOf(numero).equals(Long.valueOf(decimalDos));
+    }
+
+    /*
+    public long sumaPalindromosDecBin(){
+        int suma = 0;
+        for (long i = 0; i < 1000000; i++) {
+            if (esPalindromoBinario(i) || esPalindromoDecimal(i)){
+                suma+=i;
+            }
+        }
+        return suma;
+    }
+    */
+}
---
+++ src/tdd_tp/ejercicio8/CalculadoraPalindromoTests.java
@@ -0,0 +1,46 @@
+package tdd_tp.ejercicio8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraPalindromoTests {
+    CaculadoraPalindromo calc = new CaculadoraPalindromo();
+    @Test
+    public void cambiarBase0(){
+       int result = calc.cambiarBase(0);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    public void cambiarBase1(){
+        int result = calc.cambiarBase(1);
+        Assert.assertEquals(1,result);
+    }
+    @Test
+    public void cambiarBase2(){
+        int result = calc.cambiarBase(2);
+        Assert.assertEquals(10,result);
+    }
+    @Test
+    public void cambiarBase5(){
+        int result = calc.cambiarBase(5);
+        Assert.assertEquals(101,result);
+    }
+
+    @Test
+    public void esPalindromoDecimal0(){
+        boolean result = calc.esPalindromoDecimal(0);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void esPalindromoDecimal101(){
+        boolean result = calc.esPalindromoDecimal(101);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void esPalindromoDecimal1010(){
+        boolean result = calc.esPalindromoDecimal(1010);
+        Assert.assertFalse(result);
+    }
+
+
+}
